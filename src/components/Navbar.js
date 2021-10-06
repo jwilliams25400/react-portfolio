b6423581c9102b9a02a9534c38f8cfcb1260fee7
Jemel Williams <jwilliams25400@yahoo.com>
2021-10-05 22:13:04 -0400

    imports on navbarjs

---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+// need to make a css page an import some styling
+
+const Navbar
+
+export default Navbar() {
+    return (
+        <div className="Navbar-container">
+        <nav className="navbar navbar-expand-lg light-blue darken-4 navbar-fixed">
+        
+        
+        
+        </nav>    
+        
+        </div>
+    )
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,25 +1,33 @@
 import React from "React";
+import { Link } from " react-scroll";
+import Typed from "react-typed";
 
 // TODO
-/// need ot make css for styling 
+/// need to make css for styling
 
-const home= () => {
-    return (
-        <div id="home" className="homePage">
-            <div classname="homeIntro">
-
-                <Typed 
-                className="typed-text"
-                string={["Welcome! <br> I'm Jemel, <br> a Full stack Developer. <br> click to links to learn more about me and my journey"]}
-                typeSpeed={50}
-                backspeed={60}
-                loop
-                />
-
-            </div>
-
-
-        </div>
-    )
-}
-
+const home = () => {
+  return (
+    <div id="home" className="homePage">
+      <div classname="homeIntro">
+        <Typed
+          className="typed-text"
+          string={[
+            "Welcome! <br> I'm Jemel, <br> a Full stack Developer. <br> click to links to learn more about me and my journey",
+          ]}
+          typeSpeed={50}
+          backspeed={60}
+          loop
+        />
+        <link
+          smooth={true}
+          to="aboutMe"
+          offset={-75}
+          className="btn-main-offer"
+          href="/#aboutMe"
+        >
+          About Me
+        </link>
+      </div>
+    </div>
+  );
+};
