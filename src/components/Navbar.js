7dc875accaa0ba8f481c8e090bc724b5d684e44e
Jemel Williams <jwilliams25400@yahoo.com>
2021-10-05 22:42:12 -0400

    working on  navbarjs

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,17 +3,18 @@ import { Link } from "react-router-dom";
 
 // need to make a css page an import some styling
 
-const Navbar
-
-export default Navbar() {
+const Navbar = () => {
     return (
         <div className="Navbar-container">
         <nav className="navbar navbar-expand-lg light-blue darken-4 navbar-fixed">
-        
-        
+        <div className="nav-wrapper">
+        <h1 className="name-logo" href="/#">Jemel</h1> 
+
+        </div>    
         
         </nav>    
         
         </div>
     )
-}
+};
+export default Navbar;
