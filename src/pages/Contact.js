ece969265a7bda29dbec373af43ef174e827d25a
Jemel Williams <jwilliams25400@yahoo.com>
2021-10-06 01:11:15 -0400

    contactjs

---
+++ src/pages/Contact.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+
+
+
+const Contact = () => {
+    <div className="wrapper contact-container">
+        <div className="contact-info container">
+            <h1>Contact</h1>
+
+        <div className="contact-details row">
+            <div className="col s12">
+                <div className="row contact">
+                    
+                </div>
+            </div>
+        </div>
+
+        </div>
+
+
+
+
+
+
+
+
+
+    </div>
+}
