80ce2e57ad73833b73df80fd24e682c37caf3805
Jemel Williams <jwilliams25400@yahoo.com>
2021-10-05 23:13:02 -0400

    completed  navbarjs

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,16 +5,22 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
     return (
-        <div className="Navbar-container">
-        <nav className="navbar navbar-expand-lg light-blue darken-4 navbar-fixed">
-        <div className="nav-wrapper">
-        <h1 className="name-logo" href="/#">Jemel</h1> 
-
-        </div>    
+     <div className="Navbar-wrapper">
+       <nav className="navbar navbar-expand-lg light-blue darken-4 navbar-fixed">
+         <div className="name">
+           <h1 className="name-logo" >Jemel</h1> 
+         </div>    
+         <ul className="nav-items">
+            <li className="nav-item" id="home"><Link to="/Home" className="nav-link active" onClick={}></Link>Home</li>
+            <li className="nav-item" id="aboutMe"><Link to="/aboutme" className="nav-link active" onClick={}></Link>About Me</li>
+            <li className="nav-item" id="portfolio"><Link to="/portfolio" className="nav-link active" onClick={}></Link>Portfolio</li>
+            <li className="nav-item" id="contact"><Link to="/contact" className="nav-link active" onClick={}></Link>Contact</li>
+            
+        </ul>    
         
-        </nav>    
+      </nav>    
         
-        </div>
+     </div>
     )
 };
 export default Navbar;
