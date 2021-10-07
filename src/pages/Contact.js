264971e18afad67c52ceff262d3264be4675656c
Jemel Williams <jwilliams25400@yahoo.com>
2021-10-07 19:21:54 -0400

    contact js updates

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,28 +1,28 @@
 import React from "react";
 
-
-
-
-function Contact () {
-    return(
+function Contact() {
+  return (
     <div className="wrapper contact-container">
-        <div className="contact-info container">
-            <h2>Contact</h2>
-            <p> Interested in learning more about skills? I would enjoy learning more about the opportunity you have. Please feel free to reach out to me with the contact information listed below.<br/>
-            Thanks
-            </p>
+      <div className="contact-info container">
+        <h2>Contact</h2>
+        <p>
+          Interested in learning more about skills? I would enjoy learning more
+          about the opportunity you have. Please feel free to reach out to me
+          with the contact information listed below.
+          <br />
+          Thanks
+        </p>
+      </div>
 
-        <div className="contact-details row">
-            <div className="col s12">
-                <div className="row contact">
-                    
-                </div>
-            </div>
-        </div>
-
-        </div>
-
+      <div className="contact-details row">
+        <h4>917-941-6748</h4>
+        <br />
+        <h4>JWilliams25400@yahoo.com</h4>
+        <br />
+        <h4>JWilliams25400</h4>
+      </div>
     </div>
-)}
+  );
+}
 
 export default Contact;
