0a7e333b048a1ac88df29f308d59b7beb33c2a19
Jemel Williams <jwilliams25400@yahoo.com>
2021-10-07 21:53:35 -0400

    loaded photos and add route ot portfolio.js

---
+++ src/pages/Portfolio.js
@@ -0,0 +1,18 @@
+import React from "react";
+import fitnessTracker from "../images/fitnessTracker.jpg";
+import movieInformer from "../images/movieInformer.jpg";
+import noteTaker from "../images/noteTaker.jpg";
+import passwordGenerator from "../images/passwordGenerator.jpg";
+import refactor from "../images/refactor.jpg";
+import techBlog from "../images/techBlog.jpg";
+import timedSportsQuiz from "../images/timedSportsQuiz.jpg";
+import weatherDashboard from "../images/weatherDashboard.jpg";
+import workDayScheduler from "../images/workDayScheduler.jpg";
+
+
+
+
+
+
+
+export default Portfolio;
