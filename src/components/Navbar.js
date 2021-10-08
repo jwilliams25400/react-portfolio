9b5b1bd326576306b72276db0cfa796e54d0f1fd
Jemel Williams <jwilliams25400@yahoo.com>
2021-10-07 20:02:42 -0400

    navbar js complete

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,25 +3,68 @@ import { Link } from "react-router-dom";
 
 // need to make a css page an import some styling
 
-function Navbar () {
-    return (
-     <div className="Navbar-wrapper">
-       <nav className="navbar navbar-expand-lg light-blue darken-4 navbar-fixed">
-         <div className="name">
-           <h1 className="name-logo" >Jemel</h1> 
-         </div>    
-         <ul className="nav-items">
-            <Link to="/Home" className="nav-item" id="home">HomePage</Link>
-            <a className="nav-item" href="http://linkedin.com/in/jemel-williamsicams-a5b14014/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
-            <Link to="/aboutme" className="nav-item" id="aboutMe">About Me</Link>
-            <Link to="/portfolio" className="nav-item" id="portfolio">Portfolio</Link>
-            <Link to="/contact" className="nav-item" id="contact">Contact</Link>
-            
-        </ul>    
-        
-      </nav>    
-        
-     </div>
-    )
-};
+function Navbar() {
+  return (
+    <div className="Navbar-wrapper">
+      <nav className="navbar navbar-expand-lg light-blue darken-4 navbar-fixed">
+        <div className="name">
+          <h1 className="name-logo">Jemel</h1>
+        </div>
+        <ul className="nav-items">
+          <Link
+            to="/Home"
+            exact
+            activeClassName="active"
+            className="nav-item"
+            id="home"
+            onClick={click}
+          >
+            HomePage
+          </Link>
+          <a
+            className="nav-item"
+            exact
+            activeClassName="active"
+            href="http://linkedin.com/in/jemel-williamsicams-a5b14014/"
+            target="_blank"
+            rel="noopener noreferrer"
+            onClick={click}
+          >
+            LinkedIn
+          </a>
+          <Link
+            to="/aboutme"
+            exact
+            activeClassName="active"
+            className="nav-item"
+            id="aboutMe"
+            onClick={click}
+          >
+            About Me
+          </Link>
+          <Link
+            to="/portfolio"
+            exact
+            activeClassName="active"
+            className="nav-item"
+            id="portfolio"
+            onClick={click}
+          >
+            Portfolio
+          </Link>
+          <Link
+            to="/contact"
+            exact
+            activeClassName="active"
+            className="nav-item"
+            id="contact"
+            onClick={click}
+          >
+            Contact
+          </Link>
+        </ul>
+      </nav>
+    </div>
+  );
+}
 export default Navbar;
