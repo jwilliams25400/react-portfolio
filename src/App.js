8ca20262b21850814a9a3d7670db1c9274164f2f
Jemel Williams <jwilliams25400@yahoo.com>
2021-10-07 20:43:29 -0400

    App.js complete

--- src/App.js
+++ src/App.js
@@ -1,32 +1,29 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Navbar from "./components/Navbar"
+import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import AboutMe from "./pages/AboutMe";
 import Contact from "./pages/Contact";
-import Footer from "./components/Footer"
+import Footer from "./components/Footer";
 
 function App() {
-    return (
-        <>
-        <Navbar />
-        <Switch>
-        <Route></Route>
-        <Route></Route>
-        <Route></Route>
-        <Route></Route>
-        </Switch>
+  return (
+    <>
+      <Navbar />
 
-
-
-
-        <Footer />
-        </>
-    )
+      <Switch>
+        <Route path="/" exact>
+          <Home />
+        </Route>
+        <Route path="AboutMe" exact>
+          <AboutMe />
+        </Route>
+        <Route path="Contact" exact></Route>
+      </Switch>
+      
+      <Footer />
+    </>
+  );
 }
 
-
-
-
-
-export default App
+export default App;
