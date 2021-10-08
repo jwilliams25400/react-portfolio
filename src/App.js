a81c3dcdd2c4ed071aaa59b840ffb2f284560851
Jemel Williams <jwilliams25400@yahoo.com>
2021-10-07 20:21:44 -0400

    updated navBarjs and  footer.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Navbar from "./components/Navbar"
 import Home from "./pages/Home";
 import AboutMe from "./pages/AboutMe";
 import Contact from "./pages/Contact";
+import Footer from "./components/Footer"
 
 function App() {
     return 
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,19 @@
 import React from "react";
 
-
-
-
-
+function Footer() {
+  return (
+    <footer className="footer" id="footer-contact">
+      <div className="container is-fluid">
+        <div className="content has-text-centered col-lg-6">
+          <h4>917-941-6748</h4>
+          <br />
+          <h4>JWilliams25400@yahoo.com</h4>
+          <br />
+          <h4>JWilliams25400</h4>
+        </div>
+      </div>
+    </footer>
+  );
+}
 
 export default Footer;
